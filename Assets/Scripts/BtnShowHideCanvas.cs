592e0a260bf57b41612090febb266ab270ba0027
Antonio Cham <antonio.cham@alumnos.udg.mx>
2021-12-10 23:34:04 -0600

    Varios scripts agregados

---
+++ Assets/Scripts/BtnShowHideCanvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#pragma warning disable 0649 
+[RequireComponent(typeof(Button))]
+public class BtnShowHideCanvas : MonoBehaviour
+{
+    Button button;
+    [SerializeField] GameObject [] objectsToHide;
+    [SerializeField] GameObject objectsToShow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ChangeShowAndHide);
+    }
+
+    void ChangeShowAndHide()
+    {
+        if (objectsToShow != null)
+        {
+            objectsToShow.SetActive(true);
+        }
+
+        for (int i = 0; i < objectsToHide.Length; i++)
+        {
+            objectsToHide[i].SetActive(false);
+        }
+       
+    }
+}
