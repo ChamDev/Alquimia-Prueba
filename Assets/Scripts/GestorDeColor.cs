caffe476144710fb6a748d625bd670e1ded1459d
Antonio Cham <antonio.cham@alumnos.udg.mx>
2021-12-11 00:53:08 -0600

    Actualizacion del juego

---
+++ Assets/Scripts/GestorDeColor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestorDeColor : MonoBehaviour
+{
+   [SerializeField] private Transform circuloRotacionTransf;
+   [SerializeField] private Image [] imagenACambiarDeColor;
+   [SerializeField] private Image morado, amarillo, verde, rojo, azul, azulcielo;
+   private float rotacion;
+   
+   private void Update()
+   {
+      rotacion = circuloRotacionTransf.localEulerAngles.z;
+      Debug.Log("Rotacion: " + rotacion.ToString());
+      if (rotacion < 360 && rotacion > 300)
+      {
+         CambiarColor(morado);
+      }
+      /*
+      else if (circuloRotacionTransf.rotation.z < -90 && circuloRotacionTransf.rotation.z > -149)
+      {
+         CambiarColor(azul);
+      }
+      else if (circuloRotacionTransf.rotation.z < -150 && circuloRotacionTransf.rotation.z > -180)
+      {
+         CambiarColor(rojo);
+      }
+      else if (circuloRotacionTransf.rotation.z > 150 && circuloRotacionTransf.rotation.z < 180)
+      {
+         CambiarColor(azulcielo);
+      }
+      else if (circuloRotacionTransf.rotation.z > 40 && circuloRotacionTransf.rotation.z < 80)
+      {
+         CambiarColor(verde);
+      }
+      else if (circuloRotacionTransf.rotation.z > -25 && circuloRotacionTransf.rotation.z < 25)
+      {
+         CambiarColor(amarillo);
+      }
+      */
+   }
+
+   void CambiarColor(Image imagen)
+   {
+      for (int i = 0; i < imagenACambiarDeColor.Length; i++)
+      {
+         imagenACambiarDeColor[i].color = imagen.color;
+      }
+   }
+}
--- Assets/Scripts/SwipeMenu.cs
+++ Assets/Scripts/SwipeMenu.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 public class SwipeMenu : MonoBehaviour
 {
 
-    [SerializeField] private Transform imagenARotar;
-    
+    [SerializeField] private Transform [] imagenARotar;
+
+    [SerializeField] private float sensibilidadCursor = 0.1f, sensibilidadMobile = 1f;
+
+    [SerializeField] private MenusManager _menusManager;
     //Swipe Menu
     Vector2 firstPressPos;
     Vector2 secondPressPos;
     Vector2 currentSwipe;
-    
 
+    private float rotacion;
     private void Update()
     {
         Swipe();
@@ -21,48 +24,98 @@ public class SwipeMenu : MonoBehaviour
 
     public void Swipe()
     {
+        //Usando input touch
         if (Input.touches.Length > 0)
         {
             Touch t = Input.GetTouch(0);
             if (t.phase == TouchPhase.Began)
             {
-                //save began touch 2d point
+                //Guarda el punto inicial
                 firstPressPos = new Vector2(t.position.x, t.position.y);
             }
-            if (t.phase == TouchPhase.Ended)
+            if (t.phase == TouchPhase.Moved)
             {
-                //save ended touch 2d point
+                ////Guarda el punto final
                 secondPressPos = new Vector2(t.position.x, t.position.y);
 
-                //create vector from the two points
+                //Creando el vector entre 2 puntos
                 currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
 
-                //normalize the 2d vector
+                //normalizando el vector
                 currentSwipe.Normalize();
                 
-                //swipe left
+                //arrastre a la izquierda
                 if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
                 {
-                    LeftSwipe();
-                    //startGameManager.DogSelected = dogSelected;
+                    LeftSwipe(false);
+                   
                 }
-                //swipe right
+                
+                //arrastre a la derecha
                 if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
                 {
-                    RightSwipe();
-                    //startGameManager.DogSelected = dogSelected;
+                    RightSwipe(false);
+                   
                 }
+            }
+        }
+        
+        //Usando el Cursor
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        
+        if (Input.GetMouseButton(0))
+        {
+           
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            
+           
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+            
+            currentSwipe.Normalize();
+            
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                LeftSwipe(true);
+            }
+            
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                RightSwipe(true);
             }
         }
     }
 
-    public void LeftSwipe()
+    public void LeftSwipe(bool usandoCursor)
     {
-       
+        if (_menusManager.Index > -1 && _menusManager.Index < imagenARotar.Length - 1)
+        {
+            if (usandoCursor)
+            {
+                imagenARotar[_menusManager.Index].Rotate(0, 0, -sensibilidadCursor);
+            }
+            else
+            {
+                imagenARotar[_menusManager.Index].Rotate(0, 0, -sensibilidadMobile);
+            }
+        }
     }
 
-    public void RightSwipe()
+    public void RightSwipe(bool usandoCursor)
     {
-
+        if (_menusManager.Index > -1 && _menusManager.Index < imagenARotar.Length - 1)
+        {
+            if (usandoCursor)
+            {
+                imagenARotar[_menusManager.Index].Rotate(0, 0, sensibilidadCursor);
+            }
+            else
+            {
+                imagenARotar[_menusManager.Index].Rotate(0, 0, sensibilidadMobile);
+            }
+        }
+        
     }
 }
